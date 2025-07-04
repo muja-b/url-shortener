7773babe4ef7dfbaaa9228d1440b4c2471d64822
Muja-b <ballasmujahed@gmail.com>
2025-07-04 17:48:40 +0300

    Refactoring Colision detection method

--- app.py
+++ app.py
@@ -35,39 +35,47 @@ def index():
 def about():
     return render_template('about.html')
 
+def get_or_create_short_code(original_url):
+    short_url = generate_short_code(original_url)
+    try:
+        g.db_cur.execute("INSERT INTO urls (original_url, short_code) VALUES (%s, %s)", (original_url, short_url))
+        g.db_conn.commit()
+        return short_url, 201
+    except psycopg2.IntegrityError:
+        g.db_conn.rollback()
+        g.db_cur.execute("SELECT original_url FROM urls WHERE short_code = %s", (short_url,))
+        existing = g.db_cur.fetchone()
+        if existing and existing[0] == original_url:
+            return short_url, 200
+        # Collision: try second hash
+        alt_short_url = generate_sha256_code(original_url)
+        try:
+            g.db_cur.execute("INSERT INTO urls (original_url, short_code) VALUES (%s, %s)", (original_url, alt_short_url))
+            g.db_conn.commit()
+            return alt_short_url, 201
+        except psycopg2.IntegrityError:
+            g.db_conn.rollback()
+            g.db_cur.execute("SELECT original_url FROM urls WHERE short_code = %s", (alt_short_url,))
+            existing2 = g.db_cur.fetchone()
+            if existing2 and existing2[0] == original_url:
+                return alt_short_url, 200
+            return None, 500
+    except Exception:
+        return None, 500
+
 @app.route('/api/url', methods=['POST'])
 def post_url():
     data = request.get_json()
     if not data or 'original_url' not in data or not validators.url(data['original_url']):
         return jsonify({"error": "original_url is required and must be a valid URL"}), 400
     
-    short_url = generate_short_code(data['original_url'])
-    try:
-        g.db_cur.execute("INSERT INTO urls (original_url, short_code) VALUES (%s, %s)", (data['original_url'], short_url))
-        g.db_conn.commit()
-        return jsonify({"original_url": data['original_url'], "short_url": short_url}), 201        
-    except psycopg2.IntegrityError:
-        g.db_conn.rollback()
-        g.db_cur.execute("SELECT original_url FROM urls WHERE short_code = %s", (short_url,))
-        existing = g.db_cur.fetchone()
-        if existing and existing[0] == data['original_url']:
-            return jsonify({"original_url": data['original_url'], "short_url": short_url}), 200
-        # Collision: try second hash
-        alt_short_url = generate_sha256_code(data['original_url'])
-        try:
-            g.db_cur.execute("INSERT INTO urls (original_url, short_code) VALUES (%s, %s)", (data['original_url'], alt_short_url))
-            g.db_conn.commit()
-            return jsonify({"original_url": data['original_url'], "short_url": alt_short_url}), 201
-        except psycopg2.IntegrityError:
-            g.db_conn.rollback()
-            g.db_cur.execute("SELECT original_url FROM urls WHERE short_code = %s", (alt_short_url,))
-            existing2 = g.db_cur.fetchone()
-            if existing2 and existing2[0] == data['original_url']:
-                return jsonify({"original_url": data['original_url'], "short_url": alt_short_url}), 200
-            return jsonify({"error": "Hash collision could not be resolved."}), 500
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
+    short_url, status = get_or_create_short_code(data['original_url'])
+    if short_url and status in (200, 201):
+        return jsonify({"original_url": data['original_url'], "short_url": short_url}), status
+    elif status == 500:
+        return jsonify({"error": "Hash collision could not be resolved."}), 500
+    else:
+        return jsonify({"error": "Unknown error occurred."}), 500
 
 @app.route('/api/url/<string:short_url>', methods=['GET'])
 def get_url(short_url):
